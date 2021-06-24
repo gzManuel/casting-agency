0eb94c47af132dadd69176747e246b2d28448cbf
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-06-24 16:39:52 -0400

    docs: Refactor useHttp.js

--- frontend/src/hooks/useHttp.js
+++ frontend/src/hooks/useHttp.js
@@ -1,10 +1,8 @@
 import { useCallback, useState } from "react";
 /**
  * This hook is utilized to execute http request and get the response as an object.
- * @param  {object} requestFunction A http function of /lib.api.js to be utilized.
- * @returns {Array} Array of [response, sendRequest].
- * - response: The result of the request is an object, this result is just available after executing sendRequest function, otherwise will be an empty Array.
- * - sendRequest: Function utilized to execute the request.
+ * @param  {function} requestFunction A http function of /lib.api.js to be utilized or other kind of http request.
+ * @returns {{response:object, sendRequest:function}},
  */
 function useHttp(requestFunction) {
     //Saving the promise response into response variable.
