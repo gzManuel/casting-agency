043a58bca07ae745c9d813baccee41d49135a5d6
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-06-27 19:22:41 -0400

    feat: Add loader spinner

---
+++ frontend/src/components/UI/Spinner.js
@@ -0,0 +1,13 @@
+import './Spinner.css';
+/**
+ * Loader spinner
+ */
+const Spinner = () => {
+    return (
+        <div className='loader'>
+            
+        </div>
+    );
+};
+
+export default Spinner;
