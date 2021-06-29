673390630efbf8561e8aaaf3a9ee9e745436954d
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-06-28 20:27:51 -0400

    Refactor: Response of useHttp
    The response now also send new properties:
    	-data
    	-status
    	-error

--- frontend/src/hooks/useHttp.js
+++ frontend/src/hooks/useHttp.js
@@ -1,25 +1,60 @@
-import { useCallback, useState } from "react";
+import { useCallback, useReducer } from "react";
+
+function httpReducer(state, action) {
+    switch (action.type) {
+        case 'SEND':
+            return {
+                status: 'pending',
+                data: null,
+                error: null,
+            }
+        case 'SUCCESS':
+            return {
+                status: 'completed',
+                data: action.responseData,
+                error: null,
+            }
+        case 'ERROR':
+            return {
+                status: 'completed',
+                data: null,
+                error: action.errorMessage,
+            }
+        default:
+            return state;
+    }
+}
+
 /**
  * This hook is utilized to execute http request and get the response as an object.
  * @param  {function} requestFunction A http function of /lib.api.js to be utilized or other kind of http request.
- * @returns {{response:object, sendRequest:function}},
+ * @returns {{httpState:object, sendRequest:function}},
  */
 function useHttp(requestFunction) {
+    const init = {
+        status: 'not send',
+        data: null,
+        error: null
+    }
+    const [httpState, dispatch] = useReducer(httpReducer, init);
     //Saving the promise response into response variable.
-    const [response, setResponse] = useState([]);
     /**
      * @param x It can be the body or the uri parameter or just empty.
      */
-    const sendRequest = useCallback( async(x)=>{
-        const jsonResponse = await requestFunction(x);
-        setResponse(jsonResponse);
-    },[requestFunction]);
+    const sendRequest = useCallback(async (x) => {
+        dispatch({ type: 'SEND' });
+        try {
+            const responseData = await requestFunction(x);
+            dispatch({ type: 'SUCCESS', responseData });
+        } catch (error) {
+            dispatch({type:'ERROR',error:error.message||'Something went wrong!'});
+        }
+    }, [requestFunction]);
 
-    const r ={
-        response: response,
+    const r = {
+        httpState,
         sendRequest
     }
-
     return r;
 }
 
