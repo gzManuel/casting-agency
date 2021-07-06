399aec74011a2b4429198ebc5949a0b25e58f2d5
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-07-06 11:03:01 -0400

    refactor: Header.js refactored, auth0 takes control of the user.
    It doesn't need an authContext.js, because the auth0 sdk do this job.

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,44 +1,25 @@
-import { useEffect, useContext } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import AuthContext from '../../store/authContext';
 
 import './Header.css';
 import UserInfo from './UserInfo';
 import Spinner from '../UI/Spinner';
 
 /**
- * @todo Add code documentation
- * The the header of the web, shows the user information when is logged, and also can logout .
+ * The header of the web, shows the user information when is logged, and also can logout .
  */
 const Header = () => {
-    //This constants always are going to be reset after refresh
     //Functions
-    const { loginWithRedirect, logout, getAccessTokenSilently } = useAuth0();
+    const { loginWithRedirect, logout } = useAuth0();
     //Variables
     const { isAuthenticated, isLoading, user } = useAuth0();
 
-    //Using Context.
-    const authCtx = useContext(AuthContext);
-
     const logoutHandler = () => {
         logout();
-        authCtx.onLogout();
-        localStorage.clear();
-    }
-
-    // Once is authenticated save user data in to localstorage and login.
-    if (isAuthenticated) {
-        getAccessTokenSilently().then(token => {
-            localStorage.setItem('isAuthenticated', '1');
-            localStorage.setItem('token', token);
-            localStorage.setItem('user', JSON.stringify(user));
-        });
     }
 
     if (isLoading) {
-        //Show loading animation.
-        // return (<div>Loading...</div>);
-        return <Spinner/>
+        // Show loading animation.
+        return <Spinner />
     }
 
     return (
@@ -47,10 +28,12 @@ const Header = () => {
                 <h2 className='item__logo'>Casting agencies</h2>
             </div>
 
-            {(authCtx.isAuthenticated || isAuthenticated) && (<UserInfo name={authCtx.isAuthenticated ? authCtx.user.name : user.name}
-                picture={authCtx.isAuthenticated ? authCtx.user.picture : user.picture} />)}
+            {isAuthenticated && (<UserInfo name={user.name}
+                picture={user.picture} />)}
+
             <div className='item'>
-                {authCtx.isAuthenticated || isAuthenticated ? <button onClick={logoutHandler} className='item__logout'>Logout</button> :
+
+                {isAuthenticated ? <button onClick={logoutHandler} className='item__logout'>Logout</button> :
                     <button onClick={loginWithRedirect} className='item__login'>Login</button>}
             </div>
         </header>
