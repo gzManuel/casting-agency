7900e256919a3bf51cb068b444296f7b93ba91da
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-07-07 19:40:42 -0400

    feat: Show role into Header.

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -4,35 +4,53 @@ import './Header.css';
 import UserInfo from './UserInfo';
 import Spinner from '../UI/Spinner';
 import { useEffect } from 'react';
+import useHttp from '../../hooks/useHttp';
+import { getUserRole } from '../../lib/api';
 
 /**
  * The header of the web, shows the user information when is logged, and also can logout .
  */
 const Header = () => {
-    //Functions
+    // Functions
     const { loginWithRedirect, logout, getAccessTokenSilently } = useAuth0();
-    //Variables
+
+    // Variables
     const { isAuthenticated, isLoading, user } = useAuth0();
-
-    console.log(user);
-
+    // Getting user role
+    const { sendRequest, httpState } = useHttp(getUserRole)
+    // Setting token in localstorage and execute getUserRole request.
     useEffect(() => {
-        console.log(localStorage.getItem('token'));
-        if (localStorage.getItem('token') === null && isAuthenticated) {
-
+        // If There's not "token" in local storage and is authenticated 
+        if (isSetInLocalStorage('token') && isAuthenticated) {
+            // Get token and save in to local storage.
             getAccessTokenSilently().then(token => {
-                localStorage.setItem('token',token);
-                console.log(token);
+                localStorage.setItem('token', token);
             });
+            // Get user role
+            sendRequest(user.sub);
         }
-    }, [getAccessTokenSilently, isAuthenticated]);
+    }, [getAccessTokenSilently, isAuthenticated, sendRequest, user]);
+
+    // Check if an item is set in localStorage.
+    const isSetInLocalStorage = (item) => {
+        return localStorage.getItem(item) === null;
+    }
+
+    // Set rol in local storage to avoid request when the page is refreshed.
+    // if the status is completed and the local storage don't have the role item.
+    if (httpState.status === 'completed' && isSetInLocalStorage('role')) {
+        localStorage.setItem('role', httpState.data.role);
+    }
 
     const logoutHandler = () => {
         logout();
+        localStorage.clear();
     }
 
-    if (isLoading) {
-        // Show loading animation.
+    //isLoading == the user information is not loaded from auth0;
+    //isAuthenticated == if the user is authenticated;
+    // Show the Spinner, if the information is not loaded, or if is Authenticated and localStorage.getItem('role') === null
+    if (isLoading || (isAuthenticated && isSetInLocalStorage('role'))) {
         return <Spinner />
     }
 
@@ -42,8 +60,8 @@ const Header = () => {
                 <h2 className='item__logo'>Casting agencies</h2>
             </div>
 
-            {isAuthenticated && (<UserInfo name={user.name}
-                picture={user.picture} />)}
+            {(isAuthenticated) && (<UserInfo name={user.name}
+                picture={user.picture} role={localStorage.getItem('role')} />)}
 
             <div className='item'>
 
--- frontend/src/components/Layout/UserInfo.js
+++ frontend/src/components/Layout/UserInfo.js
@@ -6,9 +6,9 @@ import React, { Fragment } from 'react';
  * @param {string} props
  * @param {string} props.picture The picture of the user
  * @param {string} props.name The name of the user
- * @param {string} props.access The role of the user.
+ * @param {string} props.role The role of the user.
  */
-const UserInfo = ({picture, name, access}) => {
+const UserInfo = ({picture, name, role}) => {
     return (<Fragment>
         <div className='item-push-left'>
             <img className='item__user-image' src={picture} alt={name} />
@@ -20,7 +20,7 @@ const UserInfo = ({picture, name, access}) => {
                     Name:{name}
                 </li>
                 <li>
-                    Access:{access}
+                    Rol:{role}
                 </li>
             </ul>
         </div>
