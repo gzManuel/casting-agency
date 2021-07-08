97d6da8c1de7846d47fe77b7bfa8f9ead8013485
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-07-08 12:56:20 -0400

    feat: All api functions needs a token.
    Needs the api to verify if the user is able to use the api.

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -16,10 +16,16 @@ const DOMAIN = 'http://localhost:5000';
 
 /**
  * Http request to get All actors
+ * @param {string} token Token of the user
  * @returns {Promise<Actor[]>} All the actors.
  */
-export async function getAllActors() {
-    const response = await fetch(`${DOMAIN}/actors`);
+export async function getAllActors(token) {
+    const response = await fetch(`${DOMAIN}/actors`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    });
     const jsonResponse = await response.json();
     if (!jsonResponse.success) {
         throw new Error(jsonResponse.message || 'Could not fetch Actors');
@@ -29,11 +35,17 @@ export async function getAllActors() {
 
 /**
  * Http request to get an actor with the given id parameter
+ * @param {string} token Token of the user
  * @param  {number} id The id of the actor to get it.
  * @returns {Promise<Actor>} The found actor.
  */
-export async function getActor(id) {
-    const response = await fetch(`${DOMAIN}/actors/${id}`);
+export async function getActor(token, id) {
+    const response = await fetch(`${DOMAIN}/actors/${id}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    });
     const jsonResponse = await response.json();
     if (!jsonResponse.success) {
         throw new Error(jsonResponse.message || `Couldn't fetch Actor`);
@@ -42,15 +54,17 @@ export async function getActor(id) {
 }
 /**
  * Save an actor in the database.
+ * @param {string} token Token of the user
  * @param {Actor} actor
  * @returns {Promise<object>}
  */
-export async function addActor(actor) {
+export async function addActor(token, actor) {
     const response = await fetch(DOMAIN + '/actors',
         {
             method: 'Post',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
             },
             body: JSON.stringify(actor)
         });
@@ -64,14 +78,16 @@ export async function addActor(actor) {
 
 /**
  * Delete an Actor row in the database with the given id parameter.
+ * @param {string} token Token of the user
  * @param  {number} id The id of the actor to delete.
  * @returns {Promise<object>} A promise response information.
  */
-export async function deleteActor(id) {
+export async function deleteActor(token, id) {
     const response = await fetch(DOMAIN + '/actors/' + id, {
         method: 'DELETE',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
         }
     });
     const jsonResponse = await response.json();
@@ -83,10 +99,16 @@ export async function deleteActor(id) {
 }
 /**
  * Http request to get All movies
+ * @param {string} token Token of the user
  * @returns {Promise<Movie[]>} All the movies of the database.
  */
-export async function getAllMovies() {
-    const response = await fetch(DOMAIN + '/movies');
+export async function getAllMovies(token) {
+    const response = await fetch(DOMAIN + '/movies', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    });
     const jsonResponse = await response.json();
 
     if (!jsonResponse.success) {
@@ -97,11 +119,17 @@ export async function getAllMovies() {
 
 /**
  * Http request to get a movie with the given id parameter
+ * @param {string} token Token of the user
  * @param  {number} id The id of the movie to get it.
  * @returns {Promise<Movie>} The movie.
  */
-export async function getMovie(id) {
-    const response = await fetch(`${DOMAIN}/movies/${id}`);
+export async function getMovie(token, id) {
+    const response = await fetch(`${DOMAIN}/movies/${id}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    });
     const jsonResponse = await response.json();
 
     if (!jsonResponse.success) {
@@ -111,14 +139,16 @@ export async function getMovie(id) {
 }
 /**
  * Delete an Movie row in the database with the given id parameter.
+ * @param {string} token Token of the user
  * @param  {number} id The id of the movie to delete.
  * @returns {Promise<object>} A promise response information.
  */
-export async function deleteMovie(id) {
+export async function deleteMovie(token, id) {
     const response = await fetch(DOMAIN + '/movies/' + id, {
         method: 'DELETE',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
         }
     });
     const jsonResponse = await response.json();
@@ -131,14 +161,16 @@ export async function deleteMovie(id) {
 
 /**
  * Save a movie in the database.
+ * @param {string} token Token of the user
  * @param {Movie} movie. The movie to save.
  * @returns {Promise<Object>} The response.
  */
-export async function addMovie(movie) {
+export async function addMovie(token, movie) {
     const response = await fetch(`${DOMAIN}/movies`, {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
         },
         body: JSON.stringify(movie)
     });
@@ -152,15 +184,23 @@ export async function addMovie(movie) {
 
 /**
  * get rol from a user.
+ * @param {string} token Token of the user 
  * @param {string} id_user the id of the user
  * @returns {Promise<Object>} The response.
  */
-export async function getUserRole(id_user) {
-    const response = await fetch(`${DOMAIN}/users/${id_user}/role`);
+export async function getUserRole(token, id_user) {
+    const response = await fetch(`${DOMAIN}/users/${id_user}/role`,
+        {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }
+        }
+    );
 
     const jsonResponse = await response.json();
-    if (!jsonResponse.success){
-        throw new Error(jsonResponse.message||`Couldn't get user role`);
+    if (!jsonResponse.success) {
+        throw new Error(jsonResponse.message || `Couldn't get user role`);
     }
     return jsonResponse;
 }
