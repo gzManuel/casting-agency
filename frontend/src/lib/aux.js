bbfaf60dab5825eff8072f28b4fa05be9002c2e2
Manuel Alejandro Garisto Zuna <garistozunamanuel@gmail.com>
2021-07-13 18:14:54 -0400

    fix: verifyPermission don't return a correct value

--- frontend/src/lib/aux.js
+++ frontend/src/lib/aux.js
@@ -1,33 +1,49 @@
 
-export const verifyPermission = ( permission) => {
+export const verifyPermission = (permission) => {
     //Permissions of every role.
     const role = localStorage.getItem('role');
-    const castingAssistant= ['get:actors', 'get:movies', 'get:actor', 'get:movie','get:user_role'];
-    const castingDirector = ['delete:actor','post:actor','patch:actor','patch:movie'].concat([castingAssistant]);
-    const executiveProducer = ['delete:movie','post:movie'].concat(castingDirector);
+    const castingAssistant = ['get:actors', 'get:movies', 'get:actor', 'get:movie', 'get:user_role'];
+    const castingDirector = ['delete:actor', 'post:actor', 'patch:actor', 'patch:movie'].concat([castingAssistant]);
+    const executiveProducer = ['delete:movie', 'post:movie'].concat(castingDirector);
 
     switch (role) {
         case 'Casting assistant':
-            castingAssistant.forEach((element) => {
-                if(element === permission){
+            for (const element of castingAssistant) {
+                if(element===permission){
                     return true;
                 }
-            });
+            }
             return false;
+
+            // castingAssistant.forEach((element) => {
+            //     if (element === permission) {
+            //         return true;
+            //     }
+            // });
+            // return false;
         case 'Casting director':
-            castingDirector.forEach(element=>{
-                if(element === permission){
+            console.log(permission);
+            for (const element of castingDirector) {
+                if(element===permission){
                     return true;
                 }
-            });
-            return false
+            }
+            return false;
+            // castingDirector.forEach(element => {
+
+            //     if (element === permission) {
+            //         console.log(element === permission);
+            //         return true;
+            //     }
+            // });
+            // return false
         case 'Executive Producer':
-            executiveProducer.forEach(element=>{
-                if(element === permission){
+            for (const element of executiveProducer) {
+                if(element===permission){
                     return true;
                 }
-            });
-            return false
+            }
+            return false;
         default:
             return false;
     }
